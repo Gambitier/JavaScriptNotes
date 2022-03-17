000cd5db20b72143df3645aa64d2329b3f7019ab
gambitier <csegambitier@gmail.com>
2022-03-17 21:41:09 +0530

    bugfix: seeder seeded roles multiple times on app start

--- Projects/BooksLibrary/src/database/repository.interfaces/user.role.repo.interface.ts
+++ Projects/BooksLibrary/src/database/repository.interfaces/user.role.repo.interface.ts
@@ -1,6 +1,8 @@
 import { RoleDto } from 'domain.types/role/role.dto';
 
 export interface IRoleRepo {
+    search(): Promise<RoleDto[]>;
+
     create(entity: any): Promise<RoleDto>;
 
     getById(id: number): Promise<RoleDto>;
--- Projects/BooksLibrary/src/database/sql/sequelize/repositories/user.role.repo.ts
+++ Projects/BooksLibrary/src/database/sql/sequelize/repositories/user.role.repo.ts
@@ -3,11 +3,21 @@ import { Logger } from 'common/logger';
 import { IRoleRepo } from 'database/repository.interfaces/user.role.repo.interface';
 import { RoleDto } from 'domain.types/role/role.dto';
 import { Roles } from 'domain.types/role/role.types';
-import { Op } from 'sequelize/types';
 import { RoleMapper } from '../mapper/user.role.mapper';
 import Role from '../models/role.model';
 
 export class UserRoleRepo implements IRoleRepo {
+    search = async (): Promise<RoleDto[]> => {
+        try {
+            const role: Role[] = await Role.findAll();
+            const dto: RoleDto[] = role.map((userRole) => RoleMapper.toDto(userRole));
+            return dto;
+        } catch (error) {
+            Logger.instance().log(error.message);
+            throw new ApiError(500, error.message);
+        }
+    };
+
     create = async (roleEntity: any): Promise<RoleDto> => {
         try {
             const entity = {
