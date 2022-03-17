7dc2bcf7504a939955dab6bf0fb846c5ef16e1e7
gambitier <csegambitier@gmail.com>
2022-03-17 21:26:47 +0530

    set roleid while creating user

--- Projects/BooksLibrary/src/services/user.service.ts
+++ Projects/BooksLibrary/src/services/user.service.ts
@@ -1,3 +1,6 @@
+import { IRoleRepo } from 'database/repository.interfaces/user.role.repo.interface';
+import { RoleDto } from 'domain.types/role/role.dto';
+import { Roles } from 'domain.types/role/role.types';
 import { UserDomainModel } from 'domain.types/user/user.domain.model';
 import { UserDetailsDto } from 'domain.types/user/user.dto';
 import { inject, injectable } from 'tsyringe';
@@ -5,9 +8,11 @@ import { IUserRepo } from '../database/repository.interfaces/user.repo.intrerfac
 
 @injectable()
 export class UserService {
-    constructor(@inject('IUserRepo') private _userRepo: IUserRepo) {}
+    constructor(@inject('IUserRepo') private _userRepo: IUserRepo, @inject('IRoleRepo') private _roleRepo: IRoleRepo) {}
 
     create = async (userDetails: UserDomainModel): Promise<UserDetailsDto> => {
+        const userRole: RoleDto = await this._roleRepo.getByName(Roles.User);
+        userDetails.RoleId = userRole.id;
         const userDetailsDto: UserDetailsDto = await this._userRepo.createUser(userDetails);
 
         return userDetailsDto;
