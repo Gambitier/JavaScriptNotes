d10fd7a740925ea05a5ae6fd723a4b47c2244ff1
gambitier <csegambitier@gmail.com>
2022-03-20 10:51:37 +0530

    resolved rolePrivilageRepo dependency

--- Projects/BooksLibrary/src/database/sql/sql.injector.ts
+++ Projects/BooksLibrary/src/database/sql/sql.injector.ts
@@ -1,5 +1,6 @@
 import { DependencyContainer } from 'tsyringe';
 import { DatabaseConnector_Sequelize } from './sequelize/database.connector.sequelize';
+import { RolePrivilegeRepo } from './sequelize/repositories/role.privilege.repo';
 import { UserRepo } from './sequelize/repositories/user.repo';
 import { UserRoleRepo } from './sequelize/repositories/user.role.repo';
 
@@ -9,5 +10,6 @@ export class SQLInjector {
 
         container.register('IUserRepo', UserRepo);
         container.register('IRoleRepo', UserRoleRepo);
+        container.register('IRolePrivilegeRepo', RolePrivilegeRepo);
     }
 }
