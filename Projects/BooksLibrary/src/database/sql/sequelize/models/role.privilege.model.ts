60ab9f96d30f79c35e6579c83d763bdcd697958c
gambitier <csegambitier@gmail.com>
2022-03-20 12:40:54 +0530

    complete authorization implementation

--- Projects/BooksLibrary/src/database/sql/sequelize/models/role.privilege.model.ts
+++ Projects/BooksLibrary/src/database/sql/sequelize/models/role.privilege.model.ts
@@ -43,13 +43,12 @@ export default class RolePrivilege extends Model {
     })
     Privilege: string;
 
-    @IsInt
     @ForeignKey(() => Role)
     @Column({
-        type: DataType.INTEGER,
+        type: DataType.STRING,
         allowNull: false,
     })
-    RoleId: number;
+    RoleId: string;
 
     @Column
     @CreatedAt
